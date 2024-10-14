0673b983c2adabca6baaf5c43f250eb6b661b985
oliviatek <oliviatekolste@gmail.com>
2024-10-14 10:46:23 -0400

    tweaked styling for portfolio section

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,20 +1,14 @@
-export default function Card({ examples }) {
+export default function Cards({ examples }) {
     return (
         <div className="mx-auto grid w-full grid-cols-3 gap-8 pt-12 sm:w-3/4 md:gap-10">
             {examples.map((ex) => 
-                        <div className="mx-auto shadow-lg transform transition-all relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-full p-4" key={ex.id}>
-                            <h6 className="mb-2 text-darkest text-xl font-semibold">
-                            {ex.name}
-                            </h6>
-                            <ul className="list-disc ml-4">
-                                <li>{ex.pointOne}</li>
-                                <li>{ex.pointTwo}</li>
-                            </ul>
-                            <div className="relative m-2.5 overflow-hidden text-white rounded-md">
-                                <img src={ex.imgPath} alt={ex.imgAlt} />
-                            </div>
-                            <div>Check it out on <a href={ex.gitLink} target="_blank" rel="noopener noreferrer">GitHub</a> or see it <a href={ex.appLink} target="_blank" rel="noopener noreferrer">live</a></div>
-                    </div> 
+                <figure className="group mx-auto md:mx-0 relative transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0 hover:scale-105" key={ex.id}>
+                    <img className="border-2 border-darkest" src={ex.imgPath} alt="image description" />
+                <div className="absolute right-0 top-0 hidden text-white bg-darkest group-hover:block">Hi!!!</div>
+                <figcaption className="px-4 text-lg bg-darkest text-white">
+                    <p>{ex.name}</p>
+                </figcaption>
+                </figure>
             )}
     </div> 
     )
