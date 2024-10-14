045d2d4ac9a83e9343d82da26dfc9a979fa0e3ca
oliviatek <oliviatekolste@gmail.com>
2024-10-14 13:57:03 -0400

    updated portfolio to remove dummy text and fix formatting errors

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,14 +2,16 @@ export default function Cards({ examples }) {
     return (
         <div className="mx-auto grid w-full grid-cols-3 gap-8 pt-12 sm:w-3/4 md:gap-10">
             {examples.map((ex) => 
-                <figure className="group mx-auto md:mx-0 rounded-lg bg-darkest relative transition-all duration-300 cursor-pointer hover:scale-105" key={ex.id}>
-                    <img className="group-hover:opacity-0 rounded-lg" src={ex.imgPath} alt={ex.imgAlt} />
-                <figcaption className="hidden group-hover:block px-4 absolute inset-0 text-center top-1/3 text-lg text-white">
-                    <h1 className="font-semibold uppercase">{ex.name}</h1>
-                    <a href={ex.gitLink}><i className="bx bxl-github text-4xl text-white hover:text-gray-200"></i></a>
-                </figcaption>
-                </figure>
+                <div className="group md:mx-0 rounded-lg bg-darkest relative transition-all duration-300 cursor-pointer hover:scale-105" key={ex.id}>
+                    <img className="group-hover:opacity-0 h-full max-w-full rounded-lg" src={ex.imgPath} alt={ex.imgAlt}/>
+                <div className="hidden absolute inset-0 group-hover:flex flex-col items-center justify-center text-center px-4 text-m text-white">
+                    <a href={ex.appLink}><h1 className="font-semibold uppercase hover:text-mid">{ex.name}</h1></a>
+                    <h2 className="text-xs">{ex.pointTwo}</h2>
+                    <a href={ex.gitLink}><i className="bx bxl-github text-4xl text-white hover:text-mid"></i></a>
+                </div>
+                </div>
             )}
     </div> 
+
     )
 }
