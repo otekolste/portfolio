04b4a0f49a372b451ab51133c7b8bd117616c1ce
otekolste <58699635+otekolste@users.noreply.github.com>
2024-10-18 15:07:34 -0400

    added responsive design to portfolio page, fixed footer displacement on smaller screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { Outlet } from 'react-router-dom';
-import Nav from './components/NavBar';
-import Footer from './components/Footer';
+import { Outlet } from "react-router-dom";
+import Nav from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
       <Nav />
-      <main className="w-screen">
-        <Outlet />
-      </main>
-      <Footer />
+      <div className="flex flex-col">
+        <main className="w-screen">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,17 +1,29 @@
 export default function Cards({ examples }) {
-    return (
-        <div className="mx-auto grid w-full grid-cols-3 gap-8 pt-12 sm:w-3/4 md:gap-10">
-            {examples.map((ex) => 
-                <div className="group md:mx-0 rounded-lg bg-darkest relative transition-all duration-300 cursor-pointer hover:scale-105" key={ex.id}>
-                    <img className="group-hover:opacity-0 h-full max-w-full rounded-lg" src={ex.imgPath} alt={ex.imgAlt}/>
-                <div className="hidden absolute inset-0 group-hover:flex flex-col items-center justify-center text-center px-4 text-m text-white">
-                    <a href={ex.appLink}><h1 className="font-semibold uppercase hover:text-mid">{ex.name}</h1></a>
-                    <h2 className="text-xs">{ex.pointTwo}</h2>
-                    <a href={ex.gitLink}><i className="bx bxl-github text-4xl text-white hover:text-mid"></i></a>
-                </div>
-                </div>
-            )}
-    </div> 
-
-    )
+  return (
+    <div className="mx-auto grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-12 sm:w-3/4 md:gap-10">
+      {examples.map((ex) => (
+        <div
+          className="group md:mx-0 rounded-lg bg-darkest relative transition-all duration-300 cursor-pointer hover:scale-105"
+          key={ex.id}
+        >
+          <img
+            className="group-hover:opacity-0 duration-500 h-full max-w-full rounded-lg"
+            src={ex.imgPath}
+            alt={ex.imgAlt}
+          />
+          <div className="hidden absolute inset-0 group-hover:flex flex-col items-center justify-center text-center px-4 text-m text-white">
+            <a href={ex.appLink}>
+              <h1 className="font-semibold uppercase hover:text-mid">
+                {ex.name}
+              </h1>
+            </a>
+            <h2 className="text-xs">{ex.pointTwo}</h2>
+            <a href={ex.gitLink}>
+              <i className="bx bxl-github text-4xl text-white hover:text-mid"></i>
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
